18057e8827fa53a214c41ee73a8dbf28efd4b778
bordager <63132719+bordager@users.noreply.github.com>
2020-04-06 19:24:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es su nombre?");
+            string nombre;
+            nombre = Console.ReadLine();
+
+            if (nombre != "")
+            {
+                Console.WriteLine("Hola " + nombre);
+            }
+            else
+            {
+                Console.WriteLine("No te conozco");
+            }
+        }
+    }
+}
